ff3cd38c3aa5e5d3e8f11cd009a13ca0ea695987
kaja <k.santro@posteo.de>
2016-11-04 21:57:19 +0100

    fixed the static_pages_controller. now all static pages can relate to product as a class as well, yey! :)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,32 @@
 class StaticPagesController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  # GET /products
+  # GET /products.json
   def index
+    @products = Product.all
   end
 
   def landing_page
-  	@featured_product = Product.first
+    @products = Product.limit(3)
   end
+
+  # GET /products/1
+  # GET /products/1.json
+  def show
+  end
+
+  
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :description, :image_url, :color, :price)
+    end
 end
+
