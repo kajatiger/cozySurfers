732bcf11952220f587af7eedcd4f888e57ef236a
kaja <k.santro@posteo.de>
2016-11-10 17:46:15 +0100

    linked most of the buttons properly and added some styling. tried to add the social-share-button gem, but something went wrong in the routes, so i took it out again. the files are still there though

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
 	resources :orders, only: [:index, :show, :create, :destroy]
+  
+  get 'orders/index'
 
   resources :products
   get 'products/index'
@@ -8,13 +11,13 @@ Rails.application.routes.draw do
 
   get 'products/show'
 
-
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  
+
   root 'static_pages#landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
