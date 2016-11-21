a1316a02d947a71733da2d0a4f4df9cd8a618864
kaja <k.santro@posteo.de>
2016-11-21 19:06:19 +0100

    two new migrations for the comment table. scope doesnt work neither in the show.erb nor in the index.erb of the products...

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments, :dependent => :destroy
+
+	def highest_rating_comment
+  	comments.rating_desc.first
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121115332) do
+ActiveRecord::Schema.define(version: 20161121173356) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -18,7 +18,10 @@ ActiveRecord::Schema.define(version: 20161121115332) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "rating"
     t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
