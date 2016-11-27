f2022dcf2fc7dc63695976049a247a51d446e358
kaja <k.santro@posteo.de>
2016-11-27 14:24:43 +0100

    restrict delete-ability to admin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,11 +26,12 @@ class CommentsController < ApplicationController
   end
 
 	def destroy
-    @product = Product.find(params[:product_id])
-    @comment = @product.comments.find(params[:id])
+    @comment = Comment.find(params[:id])
+    product = @comment.product
     @comment.destroy
-    redirect_to product_path(@product)
+    redirect_to product
   end
+
 
 
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    cannot :destroy, Comment
   end
   
 end
