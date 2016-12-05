c68dc28b9974118ac1bd2d418850a41870e0afe6
kaja <k.santro@posteo.de>
2016-12-05 17:08:03 +0100

    redirecting the customer to orders after payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,21 +1,20 @@
 class PaymentsController < ApplicationController
  def create
-   @product  = Product.find(params[:id])
+   @product  = Product.find(params[:product_id])
    @user     = current_user
    token = params[:stripeToken]
    # Create the charge on Stripe's servers - this will charge the user's card
    charge = Stripe::Charge.create(
-     :amount => @product.price, # amount in cents, again
+     :amount => (@product.price * 100).to_i, # amount in cents, again
      :currency => "eur",
      :source => token,
      :description => params[:stripeEmail]
    )
 
    if charge.paid
-     Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
+     order = Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
+     redirect_to order_path(order)
    end
-
-   redirect_to product_path(@product)
 
    rescue Stripe::CardError => e
      body = e.json_body
@@ -24,3 +23,5 @@ class PaymentsController < ApplicationController
        # The card has been declined #payments_create_path
  end
 end
+
+
