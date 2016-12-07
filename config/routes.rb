38b2c93c448ba8e3e0cf9d0be333a9d0e2c89c85
kaja <k.santro@posteo.de>
2016-12-07 23:52:18 +0100

    created a super cool product channel so that users can always get notifications if someone posted a new comment.

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#landing_page'
 
+  mount ActionCable.server => '/cable'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
