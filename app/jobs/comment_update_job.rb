ec1c4a6faf44381650fdc3bbed24f9714a3cc3c4
kaja <k.santro@posteo.de>
2016-12-12 16:09:18 +0100

    fixed comments

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -2,13 +2,13 @@ class CommentUpdateJob < ApplicationJob
   queue_as :default
 
 
-  def perform(comment, current_user, admin_user)
-	  ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user, admin_user), average_rating: comment.product.average_rating)
+  def perform(comment, current_user)
+		ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user), average_rating: comment.product.average_rating)	
 	end
 
   private
 
-  def render_comment(comment, current_user, admin_user)
-    CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user, admin_user: admin_user})
-  end
+  def render_comment(comment, current_user)
+   CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user})
+ 	end
 end
